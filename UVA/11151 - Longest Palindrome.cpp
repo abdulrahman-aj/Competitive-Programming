82fe75667b69ccf7649d5297f49e0c0b6dd3bfce
abdulrahman-aj <45943191+abdulrahman-aj@users.noreply.github.com>
2020-03-23 06:41:19 +0200

    Update 11151 - Longest Palindrome.cpp

--- UVA/11151 - Longest Palindrome.cpp
+++ UVA/11151 - Longest Palindrome.cpp
@@ -3,31 +3,34 @@
 using namespace std;
 
 const int N = 1010;
-
-string s;
-int dp[N][N], n, t;
-
-int solve(int l, int r) {
-  assert(l <= r);
-  if (dp[l][r] != -1) return dp[l][r];
-  if (l == r) return dp[l][r] = 1;
-  if (l + 1 == r) return dp[l][r] = s[l] == s[r] ? 2 : 1;
-  dp[l][r] = max(solve(l + 1, r), solve(l, r - 1));
-  if (s[l] == s[r])
-    dp[l][r] = max(dp[l][r], 2 + solve(l + 1, r - 1));
-  return dp[l][r];
-}
+int dp[N][N], n;
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
+  int t;
   cin >> t;
   cin.ignore();
   while (t--) {
-    memset(dp, -1, sizeof(dp));
+    string s;
     getline(cin, s);
     n = s.size();
-    cout << (n == 0 ? 0 : solve(0, n - 1)) << '\n'; // empty string allowed
+    if (n == 0) {
+      cout << 0 << '\n';
+      continue;
+    }
+    for (int i = 0; i < n; ++i)
+      dp[i][i] = 1;
+    for (int i = 0; i < n - 1; ++i)
+      dp[i][i + 1] = s[i] == s[i + 1] ? 2 : 1;
+    for (int l = n - 3; l >= 0; --l) {
+      for (int r = l + 2; r < n; ++r) {
+        dp[l][r] = max(dp[l + 1][r], dp[l][r - 1]);
+        if (s[l] == s[r])
+          dp[l][r] = max(dp[l][r], 2 + dp[l + 1][r - 1]);
+      }
+    }
+    cout << dp[0][n - 1] << '\n';
   }
   return 0;
 }
